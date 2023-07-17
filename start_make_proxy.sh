31f039a3fc3ccfa14d30b7a5af8ea3015a8c781f
bougueil <rmariana@gmail.com>
2023-07-17 09:56:41 +0200

    one systemd file

--- start_make_proxy.sh
+++ start_make_proxy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-export PATH=$PATH:/usr/local/otp_25/bin
+
+# Add path to erl
+#export PATH=$PATH:/home/k/.asdf/shims
+export PATH=$PATH:/usr/local/otp/bin
+
 cd `dirname $0`
 ./_build/prod/rel/make_proxy/bin/make_proxy stop
 ./_build/prod/rel/make_proxy/bin/make_proxy daemon
