1fa5c23541e3d0ff2899ec8ac4c04727e865be22
bougueil <rmariana@gmail.com>
2023-08-30 12:12:37 +0200

    add docker compose for tests

--- test/make_proxy_test.exs
+++ test/make_proxy_test.exs
@@ -1,18 +1,18 @@
 defmodule MakeProxyTest do
   use ExUnit.Case
 
-  # # ensure a local make_proxy client and its server are started
-  # # then run :
+  @host ~c"0.0.0.0"
+  # # docker compose up -d
   # # WORKER_TYPE=make_proxy_client mix test --no-start
 
   # test "access make_proxy" do
-  #   {status, sock} = :gen_tcp.connect(~c"localhost", 7070, mode: :binary, packet: 0)
+  #   {status, sock} = :gen_tcp.connect(@host, 7070, mode: :binary, packet: 0)
   #   assert(status == :ok)
   #   :ok = :gen_tcp.close(sock)
   # end
 
   # test "example.org" do
-  #   {:ok, sock} = :gen_tcp.connect(~c"localhost", 7070, mode: :binary, packet: 0, active: false)
+  #   {:ok, sock} = :gen_tcp.connect(@host, 7070, mode: :binary, packet: 0, active: false)
   #   :ok = :gen_tcp.send(sock, "GET http://example.org/ HTTP/1.1\r\nHost: example.org\r\n\r\n")
 
   #   {:ok, body} = :gen_tcp.recv(sock, 0)
@@ -22,7 +22,7 @@ defmodule MakeProxyTest do
   # end
 
   # test "example.org/favicon.ico NOT FOUND" do
-  #   {:ok, sock} = :gen_tcp.connect(~c"localhost", 7070, mode: :binary, packet: 0, active: false)
+  #   {:ok, sock} = :gen_tcp.connect(@host, 7070, mode: :binary, packet: 0, active: false)
 
   #   :ok =
   #     :gen_tcp.send(
