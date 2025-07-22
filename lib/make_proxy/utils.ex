7dcec410b46da34260175d26617203cdd74d17ec
bougueil <rmariana@gmail.com>
2025-07-22 22:10:35 +0200

    fix gen_tcp_connect

--- lib/make_proxy/utils.ex
+++ lib/make_proxy/utils.ex
@@ -1,14 +1,11 @@
 defmodule MakeProxy.Utils do
   @moduledoc false
 
-  def connect_to_remote(),
-    do:
-      :gen_tcp.connect(
-        Application.fetch_env!(:make_proxy, :addr),
-        Application.fetch_env!(:make_proxy, :port),
-        inet_backend: :socket,
-        active: :once,
-        packet: 4,
-        binary: true
-      )
+  def connect_to_remote() do
+    :gen_tcp.connect(
+      Application.fetch_env!(:make_proxy, :remote_addr),
+      Application.fetch_env!(:make_proxy, :port),
+      [{:inet_backend, :socket}, {:active, :once}, {:packet, 4}, :binary]
+    )
+  end
 end
