0d39a5acdd587733a142a1abf3007dc43c304213
bougueil <rmariana@gmail.com>
2025-07-24 11:19:08 +0200

    port -> remote_port

--- lib/make_proxy/utils.ex
+++ lib/make_proxy/utils.ex
@@ -4,7 +4,7 @@ defmodule MakeProxy.Utils do
   def connect_to_remote() do
     :gen_tcp.connect(
       Application.fetch_env!(:make_proxy, :remote_addr),
-      Application.fetch_env!(:make_proxy, :port),
+      Application.fetch_env!(:make_proxy, :remote_port),
       [{:inet_backend, :socket}, {:active, :once}, {:packet, 4}, :binary]
     )
   end
