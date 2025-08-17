456f42aada2ccd36f2c92653c88879c45d4b82e0
bougueil <rmariana@gmail.com>
2025-08-17 15:56:42 +0200

    remove system.log

--- config/config.exs
+++ config/config.exs
@@ -7,14 +7,9 @@ config :make_proxy,
 
 config :logger, :default_handler,
   handle_otp_reports: true,
-  handle_sasl_reports: true,
-  config: [
-    file: ~c"system.log",
-    filesync_repeat_interval: 15000,
-    file_check: 15000,
-    max_no_bytes: 10_000_000,
-    max_no_files: 5,
-    compress_on_rotate: true
-  ]
+  handle_sasl_reports: true
+
+config :logger, :default_formatter,
+  format: "$time $metadata[$level] $message\n"
 
 import_config "#{config_env()}.exs"
